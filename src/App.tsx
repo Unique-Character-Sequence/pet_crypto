d91bbc701dd19217fc2dc29afaf8bebb1dd4f75b
Pilici Rustam <paroli7328129ad@gmail.com>
2023-10-23 19:18:18 +0300

    setting up basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './features/HomePage/HomePage';
 import CoinPage from './features/CoinPage/CoinPage';
 import Header from './features/Header/Header';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-const App = () => { 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const App = () => {
   return (
-    <div className='appPage'>
-      <Header />
-      <Routes>
-        <Route path='/' Component={HomePage} />
-        <Route path='/coins/:id' Component={CoinPage} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className='appPage'>
+        <Header />
+        <Routes>
+          <Route path='/' Component={HomePage} />
+          <Route path='/coins/:id' Component={CoinPage} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 };
 
