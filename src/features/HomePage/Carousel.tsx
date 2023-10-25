f839338bcc656158574d0e7aefc2b4187b382bd8
Pilici Rustam <paroli7328129ad@gmail.com>
2023-10-25 14:32:59 +0300

    on first render fetch coins w [axios]

--- src/features/HomePage/Carousel.tsx
+++ src/features/HomePage/Carousel.tsx
@@ -1,6 +1,23 @@
+import axios from "axios"
 import "./styles/Carousel.scss"
+import { TrendingCoins } from "../../api/api"
+import { useAppSelector } from "../../app/hooks";
+import { useEffect, useState } from "react";
 
 const Carousel = () => {
+  const [trending, setTrending] = useState([])
+  const currency = useAppSelector((state) => state.crypto.currencyCode);
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency))
+    setTrending(data)
+  }
+  console.log(trending)
+
+  useEffect(() => {
+    fetchTrendingCoins();
+    console.log("did Fetch")
+  }, [currency])
+
   return (
     <div className="carouselMain">Carousel</div>
   )
